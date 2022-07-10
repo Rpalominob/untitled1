4321fcfab2d6a0971462a3da53b3e26d4bf5f2a8
Ronely Palomino <u201625104@upc.edu.pe>
2022-07-10 15:37:24 -0500

    Initial commit

---
+++ src/Ejemplo_1_POO/Ejecutora.java
@@ -0,0 +1,11 @@
+package Ejemplo_1_POO;
+
+public class Trabajador{
+    private String nombre;
+    private String dni;
+    private int numeroHorasExtras;
+    private int
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
